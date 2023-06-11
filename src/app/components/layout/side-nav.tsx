10ff254a84b850001bf479cc5e51bec5064ec31a
kusaymoo <kusaymoo@vuw.ac.nz>
2023-06-11 18:32:13 +1200

    sign-in: add sign out button.

--- src/app/components/layout/side-nav.tsx
+++ src/app/components/layout/side-nav.tsx
@@ -1,13 +1,16 @@
 import { useSelector } from 'react-redux';
 import Link from "next/link";
 import { RootState } from '../../store/store';
+import SignOutButton from "../sign-out";
 
 export default function SideNav() {
   const user = useSelector((state: RootState) => state.auth.user);
   
   return (
     <nav className="fixed left-0 h-screen w-64 bg-gray-100 dark:bg-gray-900 px-2 pt-5 z-2 ">
-      {!user && (
+      {user ? (
+        <SignOutButton />
+      ) : (
         <Link href={"./login"}>
           <div className="flex py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white font-bold rounded items-center justify-center">
             Sign in
---
+++ src/app/components/sign-out.tsx
@@ -0,0 +1,32 @@
+"use strict";
+import { signOut } from "firebase/auth";
+import { logout } from "../features/auth/auth-slice";
+import { auth } from "../../../firebase";
+import { AppDispatch } from "../store/store";
+import { useDispatch } from "react-redux";
+
+export const logoutFromFirebase = () => async (dispatch: AppDispatch) => {
+  try {
+    await signOut(auth);
+    dispatch(logout());
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default function SignOutButton() {
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    logoutFromFirebase()((dispatch));
+  };
+
+  return (
+    <button
+      onClick={handleLogout}
+      className="flex py-2 px-4 bg-red-500 hover:bg-red-600 text-white font-bold rounded items-center justify-center w-full"
+    >
+      Sign out
+    </button>
+  );
+}
