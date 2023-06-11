9224ca6554c307321831433e0f9e39be60db9f7f
kusaymoo <kusaymoo@vuw.ac.nz>
2023-06-11 23:56:04 +1200

    ui_and_themes: update side nav styles.
    add shadow
    and make light mode have white background.
    update paddings so that
    nav bar doesn't go down on scroll.

--- src/app/components/layout/layout.tsx
+++ src/app/components/layout/layout.tsx
@@ -12,9 +12,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <Toaster />
       <div className="flex h-screen">
         <div className="flex flex-row flex-grow w-full">
-          <div className="pt-24">
-            <SideNav />
-          </div>
+          <SideNav />
           <main className="flex-grow min-h-screen p-24 ml-64">{children}</main>
         </div>
         <TopAppBar />
