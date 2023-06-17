706ac78cc58cbc550df360c39c77ae41a688e1be
kusaymoo <kusaymoo@vuw.ac.nz>
2023-06-18 02:15:16 +1200

    projects: highlight link if project is selected.

--- src/app/components/project-list.tsx
+++ src/app/components/project-list.tsx
@@ -5,11 +5,17 @@ import { MdAddCircle } from "react-icons/md";
 import { RootState } from "../store/store";
 import { AiOutlineFile } from "react-icons/ai";
 import FlipMove from "react-flip-move";
+import classnames from "classnames";
+import { useSearchParams } from "next/navigation";
 
 export default function ProjectList() {
   const projects = useSelector((state: RootState) => state.projects.projects);
 
   const sortedProjects = [...projects].reverse();
+
+  const currentId = useSearchParams().get("id");
+
+  console.log(currentId);
 
   return (
     <div className="flex flex-col h-full">
@@ -17,7 +23,13 @@ export default function ProjectList() {
         Projects
       </h1>
       <Link
-        className="rounded-lg bg-gray-100 dark:bg-darkColor w-full px-3 py-2 flex items-center justify-between border z-20 hover:shadow-md color-transition-applied"
+        className={classnames(
+          "rounded-lg  w-full px-3 py-2 flex items-center justify-between border z-20 hover:shadow-md color-transition-applied",
+          {
+            "bg-gray-200 dark:bg-opacity-10": !currentId,
+            "bg-white dark:bg-darkColor": currentId,
+          }
+        )}
         href={"/"}
       >
         <h2 className=" text-gray-700 font-medium dark:text-gray-200 ">
@@ -32,10 +44,18 @@ export default function ProjectList() {
         {sortedProjects.map((project) => (
           <div
             key={project.id}
-            className=" bg-white dark:bg-darkColor dark:hover:bg-darkColor color-transition-applied"
+            className="bg-white dark:bg-darkColor dark:hover:bg-darkColor color-transition-applied"
           >
             <Link href={`/project?id=${project.id}`} key={project.id}>
-              <h2 className="flex p-3 space-x-2 items-center text-gray-400 hover:text-black dark:text-gray-400 dark:hover:text-white">
+              <h2
+                className={classnames(
+                  "flex p-3 space-x-2 items-center text-gray-400 hover:text-black dark:text-gray-400 dark:hover:text-white",
+                  {
+                    "text-black dark:text-white bg-gray-50 dark:bg-opacity-10 rounded":
+                      currentId == project.id,
+                  }
+                )}
+              >
                 <AiOutlineFile size={17} />
                 <span>
                   {project.title.trim() != "" ? project.title : "New project"}
@@ -53,4 +73,15 @@ export default function ProjectList() {
       </style>
     </div>
   );
+}
+
+{
+  /* <div
+className={classnames(
+  "flex items-center justify-between w-full sticky top-0 dark:text-white transition-all duration-100",
+  {
+    "shadow-lg rounded-full bg-white dark:bg-darkColor top-[100px] py-5 px-7":
+      isScrolled,
+  }
+)} */
 }
