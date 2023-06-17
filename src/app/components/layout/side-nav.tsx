a45b984f60bab5e47652da3f0d35e58e79943ddd
kusaymoo <kusaymoo@vuw.ac.nz>
2023-06-17 23:25:54 +1200

    projects: update project list styling.

--- src/app/components/layout/side-nav.tsx
+++ src/app/components/layout/side-nav.tsx
@@ -9,30 +9,29 @@ export default function SideNav() {
   const user = useSelector((state: RootState) => state.auth.user);
   const pathName = usePathname();
   return (
-    <nav className="fixed left-0 h-screen w-64 bg-white dark:bg-darkColor shadow-xl px-2 z-2 pt-5 z-2 color-transition-applied">
-      <div className="pt-24">
-        {user && user.emailVerified ? (
-          <div className="flex flex-col space-y-4">
+    <nav className="fixed left-0 h-screen w-64 bg-white dark:bg-darkColor shadow-xl px-2 z-2 pt-32 pb-4 z-2 color-transition-applied">
+      {user && user.emailVerified ? (
+        <div className="flex flex-col h-full pb-16 space-y-4 transition-all">
+          <div className="min-h-full">
             <ProjectList />
-            <div className="border-t border-gray-200 dark:border-gray-700" />
-            <SignOutButton />
           </div>
-        ) : pathName === "/login" ||
-          pathName === "/register" ||
-          pathName === "/reset-password" ? (
-          <Link href={"./"}>
-            <div className="flex py-2 px-4 bg-blue-500 dark:bg-blue-600 dark:hover:bg-blue-500 hover:bg-blue-600 text-white font-bold rounded items-center justify-center">
-              Home
-            </div>
-          </Link>
-        ) : (
-          <Link href={"./login"}>
-            <div className="flex py-2 px-4 bg-blue-500 dark:bg-blue-600 dark:hover:bg-blue-500 hover:bg-blue-600 text-white font-bold rounded items-center justify-center">
-              Sign in
-            </div>
-          </Link>
-        )}
-      </div>
+          <SignOutButton />
+        </div>
+      ) : pathName === "/login" ||
+        pathName === "/register" ||
+        pathName === "/reset-password" ? (
+        <Link href={"./"}>
+          <div className="flex py-2 px-4 bg-blue-500 dark:bg-blue-600 dark:hover:bg-blue-500 hover:bg-blue-600 text-white font-bold rounded items-center justify-center">
+            Home
+          </div>
+        </Link>
+      ) : (
+        <Link href={"./login"}>
+          <div className="flex py-2 px-4 bg-blue-500 dark:bg-blue-600 dark:hover:bg-blue-500 hover:bg-blue-600 text-white font-bold rounded items-center justify-center">
+            Sign in
+          </div>
+        </Link>
+      )}
     </nav>
   );
 }
--- src/app/components/project-list.tsx
+++ src/app/components/project-list.tsx
@@ -3,37 +3,51 @@ import { useSelector } from "react-redux";
 import Link from "next/link";
 import { MdAddCircle } from "react-icons/md";
 import { RootState } from "../store/store";
+import { AiOutlineFile } from "react-icons/ai";
+import FlipMove from "react-flip-move";
 
 export default function ProjectList() {
   const projects = useSelector((state: RootState) => state.projects.projects);
 
+  const sortedProjects = [...projects].reverse();
+
   return (
-    <div className="flex flex-col space-y-2">
-      <h1 className="text-2xl font-bold w-full text-gray-600 dark:text-white">
+    <div className="flex flex-col h-full">
+      <h1 className="text-2xl font-bold w-full text-gray-600 ml-2 mb-3 dark:text-white">
         Projects
       </h1>
-      <div className="border-t border-gray-200 dark:border-gray-700" />
-      <button className="rounded bg-gray-100 dark:bg-gray-300 w-full px-3 py-2 flex items-center justify-between shadow-md hover:shadow-lg color-transition-applied">
-        <h2 className=" text-gray-700 font-medium dark:text-gray-800">
+      <button className="rounded-lg bg-gray-100 dark:bg-darkColor w-full px-3 py-2 flex items-center justify-between border z-20 hover:shadow-md color-transition-applied">
+        <h2 className=" text-gray-700 font-medium dark:text-gray-200 ">
           New Project
         </h2>
         <MdAddCircle
-          className="inline-block text-gray-300 dark:text-gray-400"
+          className="inline-block text-gray-400 dark:text-gray-200 "
           size={35}
         />
       </button>
-      <div className="flex flex-col space-y-2">
-        {projects.map((project) => (
-          <Link href={"/"} key={project.id}>
-            <div
-              key={project.id}
-              className=" bg-white dark:bg-gray-600 p-3 rounded-md hover:shadow-sm color-transition-applied border dark:border-none"
-            >
-              <h2>{project.title}</h2>
-            </div>
-          </Link>
+      <FlipMove className="project-list flex flex-col overflow-y-scroll border-b-[1px]">
+        {sortedProjects.map((project) => (
+          <div
+            key={project.id}
+            className=" bg-white dark:bg-darkColor dark:hover:bg-darkColor color-transition-applied"
+          >
+            <Link href={"/"} key={project.id}>
+              <h2 className="flex p-3 space-x-2 items-center text-gray-400 hover:text-black dark:text-gray-400 dark:hover:text-white">
+                <AiOutlineFile size={17} />
+                <span>
+                  {project.title.trim() != "" ? project.title : "New project"}
+                </span>
+              </h2>
+            </Link>
+          </div>
         ))}
-      </div>
+      </FlipMove>
+      <style>
+        {`
+        .project-list::-webkit-scrollbar {
+          width: 0px;
+        `}
+      </style>
     </div>
   );
 }
